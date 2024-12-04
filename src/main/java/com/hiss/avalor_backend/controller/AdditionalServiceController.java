a28949b30b882add51c3e0f95738af284e1da5a2
hiss <hissoffc@gmail.com>
2024-12-04 20:07:45 +0500

    Добавил CRUD для дополнительных услуг и перевозчиков

---
+++ src/main/java/com/hiss/avalor_backend/controller/AdditionalServiceController.java
@@ -0,0 +1,60 @@
+package com.hiss.avalor_backend.controller;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hiss.avalor_backend.entity.AdditionalService;
+import com.hiss.avalor_backend.repo.AdditionalServiceRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/additional-service")
+@RequiredArgsConstructor
+public class AdditionalServiceController {
+
+    private final AdditionalServiceRepo additionalServiceRepo;
+
+    private final ObjectMapper objectMapper;
+
+    @PreAuthorize("hasAuthority('SCOPE_READ')")
+    @GetMapping
+    public List<AdditionalService> getList() {
+        return additionalServiceRepo.findAll();
+    }
+
+    @PreAuthorize("hasAuthority('SCOPE_WRITE')")
+    @PostMapping
+    public AdditionalService create(@RequestBody AdditionalService additionalService) {
+        return additionalServiceRepo.save(additionalService);
+    }
+
+    @PreAuthorize("hasAuthority('SCOPE_WRITE')")
+    @PatchMapping("/{id}")
+    public AdditionalService patch(@PathVariable Long id, @RequestBody JsonNode patchNode) throws IOException {
+        AdditionalService additionalService = additionalServiceRepo.findById(id).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "Entity with id `%s` not found".formatted(id)));
+
+        objectMapper.readerForUpdating(additionalService).readValue(patchNode);
+
+        return additionalServiceRepo.save(additionalService);
+    }
+
+    @PreAuthorize("hasAuthority('SCOPE_DELETE')")
+    @DeleteMapping("/{id}")
+    public AdditionalService delete(@PathVariable Long id) {
+        AdditionalService additionalService = additionalServiceRepo.findById(id).orElse(null);
+        if (additionalService != null) {
+            additionalServiceRepo.delete(additionalService);
+        }
+        return additionalService;
+    }
+}
---
+++ src/main/java/com/hiss/avalor_backend/controller/CarrierController.java
@@ -0,0 +1,64 @@
+package com.hiss.avalor_backend.controller;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hiss.avalor_backend.entity.Carrier;
+import com.hiss.avalor_backend.repo.CarrierRepo;
+import com.hiss.avalor_backend.service.CarrierService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/carrier")
+@RequiredArgsConstructor
+public class CarrierController {
+
+    private final CarrierService carrierService;
+
+    private final CarrierRepo carrierRepo;
+
+    private final ObjectMapper objectMapper;
+
+    @PreAuthorize("hasAuthority('SCOPE_WRITE')")
+    @PostMapping
+    public Carrier create(@RequestBody Carrier carrier) {
+        return carrierRepo.save(carrier);
+    }
+
+    @PreAuthorize("hasAuthority('SCOPE_READ')")
+    @GetMapping
+    public List<Carrier> getList() {
+        return carrierRepo.findAll();
+    }
+
+    @PreAuthorize("hasAuthority('SCOPE_WRITE')")
+    @PatchMapping("/{id}")
+    public Carrier patch(@PathVariable Long id, @RequestBody JsonNode patchNode) throws IOException {
+        Carrier carrier = carrierRepo.findById(id).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "Entity with id `%s` not found".formatted(id)));
+
+        objectMapper.readerForUpdating(carrier).readValue(patchNode);
+
+        return carrierRepo.save(carrier);
+    }
+
+    @PreAuthorize("hasAuthority('SCOPE_DELETE')")
+    @DeleteMapping("/{id}")
+    public Carrier delete(@PathVariable Long id) {
+        Carrier carrier = carrierRepo.findById(id).orElse(null);
+        if (carrier != null) {
+            carrierRepo.delete(carrier);
+        }
+        return carrier;
+    }
+}
