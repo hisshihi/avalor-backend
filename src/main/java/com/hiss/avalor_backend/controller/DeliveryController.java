f6ef1c2e7a3db013b308e4be11a735ac0a9d96c2
hiss <hissoffc@gmail.com>
2024-12-04 10:03:38 +0500

    Добавил вывод id для сегментов

--- src/main/java/com/hiss/avalor_backend/controller/DeliveryController.java
+++ src/main/java/com/hiss/avalor_backend/controller/DeliveryController.java
@@ -58,6 +58,7 @@ public class DeliveryController {
                             }
 
                             return new RouteSegmentDto(
+                                    r.getId(),
                                     r.getCityFrom(),
                                     r.getCityTo(),
                                     r.getCarrier().getName(),
--- src/main/java/com/hiss/avalor_backend/dto/RouteSegmentDto.java
+++ src/main/java/com/hiss/avalor_backend/dto/RouteSegmentDto.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 @Setter
 public class RouteSegmentDto {
 
+    Long id;
     String cityFrom;
     String cityTo;
     String carrier;
@@ -17,7 +18,8 @@ public class RouteSegmentDto {
     String pol;
     String pod;
 
-    public RouteSegmentDto(String cityFrom, String cityTo, String carrier, int cost, String transportType, String containerTypeSize, String filo, String pol, String pod) {
+    public RouteSegmentDto(Long id, String cityFrom, String cityTo, String carrier, int cost, String transportType, String containerTypeSize, String filo, String pol, String pod) {
+        this.id = id;
         this.cityFrom = cityFrom;
         this.cityTo = cityTo;
         this.carrier = carrier;
