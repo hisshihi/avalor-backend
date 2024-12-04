1a4257c28aad0c9b1ee4fb56328afe2faed317d5
hiss <hissoffc@gmail.com>
2024-12-04 21:25:38 +0500

    Добавил пагинацию к репозиторию

--- src/main/java/com/hiss/avalor_backend/repo/ApplicationRepo.java
+++ src/main/java/com/hiss/avalor_backend/repo/ApplicationRepo.java
@@ -2,12 +2,13 @@ package com.hiss.avalor_backend.repo;
 
 import com.hiss.avalor_backend.entity.Application;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ApplicationRepo extends JpaRepository<Application, Long> {
+public interface ApplicationRepo extends JpaRepository<Application, Long>, PagingAndSortingRepository<Application, Long> {
 
     List<Application> findByCreatedById(Long id);
 
