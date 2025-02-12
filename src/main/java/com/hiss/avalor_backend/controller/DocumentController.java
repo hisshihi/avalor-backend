5c323b8cfb66770c56b6f4f4ea789eaca34ed9ca
hisshihi <hissoffc@gmail.com>
2025-02-13 00:38:03 +0500

    Поменял метод кончной точки

--- src/main/java/com/hiss/avalor_backend/controller/DocumentController.java
+++ src/main/java/com/hiss/avalor_backend/controller/DocumentController.java
@@ -11,10 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriUtils;
 
 import java.io.IOException;
@@ -81,7 +78,7 @@ public class DocumentController {
 //    }
 
     @PreAuthorize("hasAnyAuthority('SCOPE_READ')")
-    @GetMapping
+    @PostMapping
     public ResponseEntity<Resource> generateDocument(@RequestBody DocumentContactDto contactDto) {
         log.info(contactDto.toString());
         try {
